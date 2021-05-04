64dc1cf20a27eb14acb06f6ca1db924dc83f7afa
kariman <kariman0800@gmail.com>
2021-05-04 20:10:36 +0200

    read,delete blog

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -180,11 +180,35 @@ exports.listAllCategoriesTAgs = (req, res) => {
 }
 
 exports.read = (req, res) => {
-    const slug = req.body.params.slug.toLowerCase()
+    const slug = req.params.slug.toLowerCase();
+    Blog.findOne({slug})
+    .populate('categories', '_id name slug')
+    .populate('tags', '_id name slug')
+    .populate('postedBy', '_id name username')
+    .select('_id title body slug mtitle mdesc categories tags postedBy createdAt updatedAt ')
+    .exec((err, data)=>{
+        if(err){
+            return res.json({
+                error : errorHandler(err)
+            })
+        }
+        res.json(data)
+    })
 }
 
 exports.remove = (req, res) => {
+    const slug = req.params.slug.toLowerCase();
+    Blog.findOneAndRemove({slug}).exec((err, data)=>{
+        if(err) {
+            return res.json({
+                error : errorHandler(err)
+            })
+        }
 
+        res.json({
+            message : 'Blog deleted successfully '
+        })
+    })
 }
 
 exports.update = (req, res) => {
