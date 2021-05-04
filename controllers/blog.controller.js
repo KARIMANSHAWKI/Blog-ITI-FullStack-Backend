696a23b1a6e8bb40d9b92befb471136394f51ed0
kariman <kariman0800@gmail.com>
2021-05-04 19:07:46 +0200

    list blog 's categories and tags

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -107,9 +107,87 @@ exports.create = (req, res) => {
 
         })
         })
-
-
-
     })
+}
+
+// *********************** Operations on blog ***********************
+exports.list = (req, res) => {
+    Blog.find({})
+    .populate('categories', '_id name slug')
+    .populate('tags', '_id name slug')
+    .populate('postedBy', '_id name username')
+    .select('_id title slug excerpt categories tags postedBy createdAt updatedAt')
+    .exec((err, data)=>{
+        if(err){
+            return res.json({
+                error : errorHandler(err)
+            })
+        }
+
+        res.json(data)
+    })
+}
+
+exports.listAllCategoriesTAgs = (req, res) => {
+    let limit = req.body.limit ? parent(req.body.limit) : 10;
+    let skip = req.body.skip ? parseInt(req.body.skip) : 0;
+
+    let blogs
+    let categories
+    let tags
+        // ********** get blogs ********
+    Blog.find({})
+    .populate('categories', '_id name slug')
+    .populate('tags', '_id name slug')
+    .populate('postedBy', '_id name username profile')
+    .sort({createdAt : -1})
+    .skip(skip)
+    .limit(limit)
+    .select('_id title slug excerpt categories tags postedBy createdAt updatedAt')
+    .exec((err, data)=>{
+        if(err){
+            return res.json({
+                error : errorHandler(err)
+            })
+        }
+
+        blogs = data;
+
+      // ******* get categories *******
+      Category.find({}).exec((err, c)=>{
+        if(err){
+            return res.json({
+                error : errorHandler(err)
+            })
+        }
+        categories = c
+
+        // ****** get categories ****** 
+        Tag.find({}).exec((err, t)=>{
+            if(err){
+                return res.json({
+                    error : errorHandler(err)
+                })
+            }
+
+            tags = t
+
+            res.json({blogs, categories, tags, size: blogs.length})
+        })
+
+      })
+    })
+}
+
+exports.read = (req, res) => {
+
+}
+
+exports.remove = (req, res) => {
+
+}
+
+exports.update = (req, res) => {
+
 }
 
--- routes/blog.route.js
+++ routes/blog.route.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const {create} = require('../controllers/blog.controller')
+const {create, list, listAllCategoriesTAgs, read, remove, update} = require('../controllers/blog.controller')
 const { adminMiddleWare , requireSignin} = require("../controllers/auth.controller");
 
 
 router.post('/blog', requireSignin, adminMiddleWare, create)
+router.get('/blogs', list)
+router.get('/blogs-categories-tags', listAllCategoriesTAgs)
+router.get('/blog/:slug', read)
+router.delete('/blog/:slug',requireSignin,adminMiddleWare, remove)
+router.put('/blog/:slug',requireSignin,adminMiddleWare, update)
+
+
+
+
+
 
 module.exports = router
